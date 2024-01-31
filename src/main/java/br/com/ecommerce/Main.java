d008f7198fef2899b37d530c3c03b2d5e1242ffa
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2024-01-31 11:55:52 -0300

    feat: entrypoint singleton and start stop consumers

--- src/main/java/br/com/ecommerce/Main.java
+++ src/main/java/br/com/ecommerce/Main.java
@@ -4,15 +4,11 @@ import br.com.ecommerce.core.order.NewOrder;
 import br.com.ecommerce.infra.EntryPoint;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
 
     public static final int SLEEP_TIME = 5000;
-    public static final String STOP = "c";
-    public static int count = 0;
 
     public static void main(String[] args) {
 
@@ -23,15 +19,15 @@ public class Main {
                 "15,16,17,18"
         );
 
-        var entrypoint = EntryPoint.listening();
+        EntryPoint.getInstance().start();
 
         messages.forEach(order -> {
             sleep();
-            NewOrder.sendMessage(order, "[Email content 0" + count++ + "]");
+            NewOrder.sendMessage(order, "EMAIL VALUE");
             sleep();
         });
 
-        entrypoint.stop();
+        EntryPoint.getInstance().stop();
     }
 
     public static void sleep() {
@@ -45,11 +41,5 @@ public class Main {
     public static int getRandomNumber() {
         Random random = new Random();
         return random.nextInt(SLEEP_TIME) + 1;
-    }
-
-    public static void threadStarting(Class<?> javaClass) {
-        System.out.println("-----------------------------");
-        System.out.println("Starting a thread ::: " + javaClass.getName());
-        System.out.println("-----------------------------");
     }
 }
--- src/main/java/br/com/ecommerce/infra/EntryPoint.java
+++ src/main/java/br/com/ecommerce/infra/EntryPoint.java
@@ -3,31 +3,33 @@ package br.com.ecommerce.infra;
 import br.com.ecommerce.application.service.EmailService;
 import br.com.ecommerce.application.service.FraudDetectorService;
 import br.com.ecommerce.application.service.LogService;
+import br.com.ecommerce.application.service.contract.AbstractKafkaConsumer;
+
+import java.util.List;
 
 public class EntryPoint {
 
     private static final EntryPoint INSTANCE = new EntryPoint();
-    private final EmailService emailService;
-    private final FraudDetectorService fraudDetectorService;
-    private final LogService logService;
+
+    private final List<AbstractKafkaConsumer> consumers;
 
     private EntryPoint() {
-        this.emailService = new EmailService();
-        this.fraudDetectorService = new FraudDetectorService();
-        this.logService = new LogService();
+        this.consumers = List.of(
+                new EmailService(),
+                new FraudDetectorService(),
+                new LogService()
+        );
     }
 
     public static EntryPoint getInstance() {
         return INSTANCE;
     }
-    public void stop() {
-        emailService.interrupt();
-        fraudDetectorService.interrupt();
-        logService.interrupt();
-    }
+
     public void start() {
-        emailService.start();
-        fraudDetectorService.start();
-        logService.start();
+        consumers.forEach(AbstractKafkaConsumer::start);
+    }
+
+    public void stop() {
+        consumers.forEach(AbstractKafkaConsumer::interrupt);
     }
 }
